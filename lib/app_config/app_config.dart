04cc7c9f34ad06d53262069285b9b27fa7649212
komodoce <komodoce23@gmail.com>
2025-02-04 05:00:17 +0000

    CE

--- lib/app_config/app_config.dart
+++ lib/app_config/app_config.dart
@@ -45,7 +45,7 @@ class AppConfig {
 
   // Brand config below
 
-  String get appName => 'Komodo Wallet CE';
+  String get appName => 'Komodo Wallet';
   String get appCompanyLong => 'Komodo Platform';
   String get appCompanyShort => 'Komodo';
 
